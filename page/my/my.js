f2e03493bf414861c3cd14a7f73214f0cdffcfe9
---谭锦威 <374055608@qq.com>
2017-12-20 14:19:57 +0800

    readme.md

--- page/my/my.js
+++ page/my/my.js
@@ -1,15 +1,34 @@
 const util = require("../../utils/utils.js");
+const app = getApp();
 Page({
 	data:{
-		userinfo:[]
+		userinfo:[],
+		userInfo:{}
 	},
 	onLoad(){
-		const _this= this;
-		util.getUserInfo({}).then((res)=>{
-			console.log(res);
-			_this.setData({
-				userinfo:res.data
-			})
-		})
+		if (app.globalData.userInfo) {
+	      this.setData({
+	        userInfo: app.globalData.userInfo,
+	        hasUserInfo: true
+	      })
+	    } else {
+	      // 在没有 open-type=getUserInfo 版本的兼容处理
+	      wx.getUserInfo({
+	        success: res => {
+	          app.globalData.userInfo = res.userInfo
+	          this.setData({
+	            userInfo: res.userInfo,
+	            hasUserInfo: true
+	          })
+	        }
+	      })
+	    }
+		// const _this= this;
+		// util.getUserInfo({}).then((res)=>{
+		// 	console.log(res);
+		// 	_this.setData({
+		// 		userinfo:res.data
+		// 	})
+		// })
 	}
 })
