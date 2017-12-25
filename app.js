156fe051c85931c380135bf94a5c3ba7e96a42c7
---谭锦威 <374055608@qq.com>
2017-12-25 11:58:51 +0800

    my

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
+//app.js
 App({
-	onLaunch(){
-		console.log('onLaunch');
-	},
-	onLoad(){
-		wx.login({
-			 success: res => {
-			 	const code = res.code;
-			 	if (code) {
-			 		console.log('获取用户登录凭证：'+code)
-			 	}else{
-			 		console.log('获取用户登录态失败：' + res.errMsg)
-			 	}
-        		// 发送 res.code 到后台换取 openId, sessionKey, unionId
-     	 	}
-		})
-		wx.getUserInfo({
-			success:(res)=>{
-				if(res.authSetting['scope.record']){
-					wx.getUserInfo({
-						success:(res)=>{
-							this.globalData.userInfo=res.userInfo
-							if (this.userInfoReadyCallback) {
-				                this.userInfoReadyCallback(res)
-				            }
-						}
-					})
-				}
-			}
-		})
-	},
-	globalData: {
-	    userInfo: null
-	 }
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
 })
